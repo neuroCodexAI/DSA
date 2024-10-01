24a0c75e2936d4d60350d0a3e05c4d152700d368
bvikash363R <bvikash363@gmail.com>
2024-10-01 21:07:26 +0530

    print N to 1

--- Recursion/Number_print.java
+++ Recursion/Number_print.java
@@ -1,1 +1,18 @@
-// Print Numbers from N to M;
+
+// Print number 1 to Nth no.
+import java.util.*;
+
+public class Number_Print {
+    public static void print_number_n_to_1(int n) {
+        if (n == 0) {
+            return;
+        }
+        System.out.println(n);
+        print_number_n_to_1(n - 1);
+    }
+
+    public static void main(String[] args) {
+        int n = 5;
+        print_number_n_to_1(n);
+    }
+}

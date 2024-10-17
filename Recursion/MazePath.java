022e1b09e5f79429fc9cc33c1babfaceef037cdb
bvikash363R <bvikash363@gmail.com>
2024-10-17 06:28:25 +0530

    Maze path count  Problems

---
+++ Recursion/MazePath.java
@@ -0,0 +1,17 @@
+public class MazePath {
+    public static int mazeCount(int i, int j, int n, int m) {
+        if (i == n || j == m) {
+            return 0;
+        }
+        if (i == n - 1 && j == m - 1) {
+            return 1;
+        }
+        int downWord = mazeCount(i + 1, j, n, m);
+        int forWord = mazeCount(i, j + 1, n, m);
+        return downWord + forWord;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(mazeCount(0, 0, 3, 3));
+    }
+}

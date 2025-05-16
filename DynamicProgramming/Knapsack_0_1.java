821af9a89d26da87f34b24fb10d0ef3026642dff
Vikash Bharti <bvikash363@gmail.com>
2025-05-16 11:46:36 +0530

    Solve knapsack problem using tabulation techniques

--- DynamicProgramming/Knapsack_0_1.java
+++ DynamicProgramming/Knapsack_0_1.java
@@ -58,6 +58,39 @@ public class Knapsack_0_1 {
         }
     }
 
+    // Tabulation
+    public static int knapsack_tabulation(int[] val, int[] wt, int W) {
+        int[][] dp = new int[val.length + 1][W + 1];
+        int n = val.length;
+        for (int i = 0; i < dp.length; i++) {
+            dp[i][0] = 0;
+        }
+
+        for (int j = 0; j < dp[0].length; j++) {
+            dp[0][j] = 0;
+        }
+
+        for (int i = 1; i < val.length + 1; i++) {
+            for (int j = 1; j < W + 1; j++) {
+                int v = val[i - 1];
+                int w = wt[i - 1];
+                if (w <= j) {
+                    int incProfit = v + dp[i - 1][j - w];
+
+                    int excProfit = dp[i - 1][j];
+
+                    dp[i][j] = Math.max(incProfit, excProfit);
+
+                } else {
+                    int excProfit = dp[i - 1][j];
+                    dp[i][j] = excProfit;
+                }
+            }
+        }
+
+        return dp[n][W];
+    }
+
     public static void main(String[] args) {
         int val[] = { 15, 14, 10, 45, 30 };
         int wt[] = { 2, 5, 1, 3, 4 };
@@ -65,5 +98,6 @@ public class Knapsack_0_1 {
 
         System.out.println(knapsack(val, wt, W, val.length));
         System.out.println(knapsack_memo(val, wt, W));
+        System.out.println(knapsack_tabulation(val, wt, W));
     }
 }
